f3235b4403e496bb41582b850083e8dd1d5cf103
Rob Svirskas <svirskasr@janelia.hhmi.org>
2020-06-29 14:16:17 -0400

    Work on denormalize_s3 for ancillary directories

--- bin/denormalize_s3.py
+++ bin/denormalize_s3.py
@@ -17,6 +17,7 @@ CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
 AWS = dict()
 KEYFILE = "keys_denormalized.json"
 COUNTFILE = "counts_denormalized.json"
+TAGS = 'PROJECT=CDCS&STAGE=prod&DEVELOPER=svirskasr&VERSION=%s' % (__version__)
 
 
 def call_responder(server, endpoint):
@@ -65,6 +66,22 @@ def get_all_s3_objects(s3c, **base_kwargs):
         continuation_token = response.get('NextContinuationToken')
 
 
+def upload_to_aws(s3r, body, object_name):
+    if ARG.TEST:
+        LOGGER.info("Would have uploaded %s", object_name)
+        return
+    LOGGER.info("Uploading %s", object_name)
+    try:
+        bucket = s3r.Bucket(ARG.BUCKET)
+        bucket.put_object(Body=body,
+                          Key=object_name,
+                          ACL='public-read',
+                          ContentType='application/json',
+                          Tagging=TAGS)
+    except ClientError as err:
+        LOGGER.error(str(err))
+
+
 def denormalize():
     """ Denormalize a buckek into a JSON file
         Keyword arguments:
@@ -73,7 +90,7 @@ def denormalize():
           None
     """
     #pylint: disable=no-member
-    total_objects = 0
+    total_objects = dict()
     sts_client = boto3.client('sts')
     aro = sts_client.assume_role(RoleArn=AWS['role_arn'],
                                  RoleSessionName="AssumeRoleSession1")
@@ -87,38 +104,35 @@ def denormalize():
                                  aws_secret_access_key=credentials['SecretAccessKey'],
                                  aws_session_token=credentials['SessionToken'])
     prefix = '/'.join([ARG.TEMPLATE, ARG.LIBRARY]) + '/'
-    key_list = list()
+    key_list = dict()
+    separate = ['searchable_neurons']
     for obj in get_all_s3_objects(s3_client, Bucket=ARG.BUCKET, Prefix=prefix):
-        if KEYFILE not in obj['Key'] and COUNTFILE not in obj['Key']:
-            total_objects += 1
-            key_list.append(obj['Key'])
-    if not total_objects:
+        which = 'default'
+        for searchdir in separate:
+            if searchdir in obj['Key']:
+                which = searchdir
+                continue
+        if KEYFILE in obj['Key'] or COUNTFILE in obj['Key']:
+            continue
+        LOGGER.info(obj['Key'])
+        if which not in key_list:
+            key_list[which] = list()
+            total_objects[which] = 0
+        total_objects[which] += 1
+        key_list[which].append(obj['Key'])
+    if not total_objects['default']:
         LOGGER.error("%s/%s was not found in the %s bucket", ARG.TEMPLATE, ARG.LIBRARY, ARG.BUCKET)
         sys.exit(-1)
-    tags = 'PROJECT=CDCS&STAGE=prod&DEVELOPER=svirskasr&VERSION=%s' % (__version__)
-    object_name = '/'.join([ARG.TEMPLATE, ARG.LIBRARY, KEYFILE])
-    LOGGER.info("Uploading %s", object_name)
-    try:
-        bucket = s3_resource.Bucket(ARG.BUCKET)
-        bucket.put_object(Body=json.dumps(key_list, indent=4),
-                          Key=object_name,
-                          ACL='public-read',
-                          ContentType='application/json',
-                          Tagging=tags)
-    except ClientError as err:
-        LOGGER.error(str(err))
-    object_name = '/'.join([ARG.TEMPLATE, ARG.LIBRARY, COUNTFILE])
-    LOGGER.info("Uploading %s", object_name)
-    try:
-        bucket = s3_resource.Bucket(ARG.BUCKET)
-        bucket.put_object(Body=json.dumps({"objectCount": total_objects}, indent=4),
-                          Key=object_name,
-                          ACL='public-read',
-                          ContentType='application/json',
-                          Tagging=tags)
-    except ClientError as err:
-        LOGGER.error(str(err))
-    print("Objects: %d" % total_objects)
+    # Write files
+    for which in ['default', 'searchable_neurons']:
+        prefix = '/'.join([ARG.TEMPLATE, ARG.LIBRARY])
+        if which != 'default':
+            prefix += '/' + which
+        object_name = '/'.join([prefix, KEYFILE])
+        print("%s objects: %d" % (which, total_objects[which]))
+        upload_to_aws(s3_resource, json.dumps(key_list, indent=4), object_name)
+        object_name = '/'.join([prefix, COUNTFILE])
+        upload_to_aws(s3_resource, json.dumps(key_list, indent=4), object_name)
 
 
 if __name__ == '__main__':
@@ -129,6 +143,8 @@ if __name__ == '__main__':
                         default='JRC2018_Unisex_20x_HR', help='Template')
     PARSER.add_argument('--library', dest='LIBRARY', action='store',
                         default='Vienna_Gen1_LexA', help='Library')
+    PARSER.add_argument('--test', dest='TEST', action='store_true',
+                        default=False, help='Test mode (do not write to bucket)')
     PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
                         default=False, help='Flag, Chatty')
     PARSER.add_argument('--debug', dest='DEBUG', action='store_true',
